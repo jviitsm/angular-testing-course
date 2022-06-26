e4eab347bbae7dd77b26f53b13ba45cbcbfffff2
joao modesto <jvictorgmodesto@gmail.com>
2022-06-26 16:56:23 -0300

    testes de card-list-component finalizados

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -16,6 +16,7 @@ import { setupCourses } from "../common/setup-test-data";
 describe("CoursesCardListComponent", () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -25,6 +26,7 @@ describe("CoursesCardListComponent", () => {
       .then(() => {
         fixture = TestBed.createComponent(CoursesCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement;
       });
   }));
 
@@ -33,10 +35,26 @@ describe("CoursesCardListComponent", () => {
   });
 
   it("should display the course list", () => {
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+
+    const cards = el.queryAll(By.css(".course-card"));
+    expect(cards).toBeTruthy();
+    expect(cards.length).toBe(12);
   });
 
   it("should display the first course", () => {
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+
+    const course = component.courses[0];
+
+    const card = el.query(By.css(".course-card:first-child")),
+      title = card.query(By.css("mat-card-title")),
+      image = card.query(By.css("img"));
+
+    expect(card).toBeTruthy();
+    expect(title.nativeElement.innerText).toBe(course.titles.description);
+    expect(image.nativeElement.src).toBe(course.iconUrl);
   });
 });
